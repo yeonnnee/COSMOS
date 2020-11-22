36822e60a8b4ce1083c8013e3769517bd34f7d93
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-11-22 23:13:49 +0900

    [CREATE] initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <h2>hello</h2>
+        </div>
+    );
+}
+
+export default App;
