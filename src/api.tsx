bf3c62244117e75d96bead7071d07e99fbb97c09
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-11-24 19:46:35 +0900

    [CREATE] create axios

--- src/api.tsx
+++ src/api.tsx
@@ -1,11 +1,26 @@
-import axios from "axios";
+import axios, { AxiosPromise } from "axios";
 
 const api_key = process.env.REACT_APP_API_KEY;
 const api = axios.create({
-    baseURL: `https://api.nasa.gov/planetary/apod`,
+    baseURL: `https://api.nasa.gov`,
     params: {
         api_key: api_key,
     },
 });
 
+interface INasaApi {
+    apod: () => AxiosPromise;
+    insight: () => AxiosPromise;
+}
+
+export const nasaApi: INasaApi = {
+    apod: () => api.get("/planetary/apod"),
+    insight: () =>
+        api.get("/insight_weather/", {
+            params: {
+                feedtype: "json",
+                ver: "1.0",
+            },
+        }),
+};
 export default api;
