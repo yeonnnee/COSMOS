8ef6b5d5cd70b70f427d51f1161fcd1aabc1e487
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-11-24 19:49:12 +0900

    [CREATE] create homeContainer and fetchData func to get today's pic

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { nasaApi } from "../../api";
 
 import HomePresenter from "./HomePresenter";
 
 const Home: () => JSX.Element = () => {
-    return <HomePresenter />;
+    const [apod, setApod] = useState({
+        date: "",
+        explanation: "",
+        hdurl: "",
+        title: "",
+    });
+
+    async function FetchData() {
+        try {
+            const res = await nasaApi.apod();
+            setApod({
+                date: res.data.date,
+                explanation: res.data.explanation,
+                hdurl: res.data.hdurl,
+                title: res.data.title,
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        FetchData();
+    }, []);
+    return <HomePresenter apod={apod} />;
 };
 
 export default Home;
