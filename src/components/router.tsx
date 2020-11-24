a9bb1c5f5b6bbcc489e784037eb2d618d85e2825
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-11-24 19:51:08 +0900

    [CREATE] create today page router

--- src/components/router.tsx
+++ src/components/router.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import Cover from "../pages/Cover";
+import Home from "../pages/Home";
 import Navigation from "./Navigation";
 
 const Router: () => JSX.Element = () => {
@@ -9,6 +10,7 @@ const Router: () => JSX.Element = () => {
         <BrowserRouter>
             <Navigation />
             <Route path="/" exact component={Cover} />
+            <Route path="/today" exact component={Home} />
         </BrowserRouter>
     );
 };
