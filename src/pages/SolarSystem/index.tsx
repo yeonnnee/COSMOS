cd2d18655b4c977c4ffa519dc3e8ce47ad875e30
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-11-27 00:14:37 +0900

    [CREATE] create getPrevItem func

--- src/pages/SolarSystem/index.tsx
+++ src/pages/SolarSystem/index.tsx
@@ -6,24 +6,51 @@ const SolarSystem: () => JSX.Element = () => {
         currentGroup: 1,
         left: 0,
         max: false,
+        min: true,
     });
 
     function getNextItem() {
         if (state.currentGroup < 5) {
             setState({
                 currentGroup: state.currentGroup + 1,
-                left: state.currentGroup * 350 + 20 * state.currentGroup,
+                left: -state.currentGroup * 350 + -20 * state.currentGroup,
                 max: false,
+                min: false,
             });
         } else {
             setState({
                 currentGroup: state.currentGroup,
                 left: state.left,
                 max: true,
+                min: false,
             });
         }
     }
-    return <SolarSystemPresneter state={state} getNextItem={getNextItem} />;
+    function getPrevItem() {
+        if (state.currentGroup === 1) {
+            setState({
+                currentGroup: state.currentGroup,
+                left: state.left,
+                max: false,
+                min: true,
+            });
+        } else {
+            setState({
+                currentGroup: state.currentGroup - 1,
+                left: state.left + 370,
+                max: true,
+                min: false,
+            });
+        }
+    }
+    console.log(state.currentGroup);
+    return (
+        <SolarSystemPresneter
+            state={state}
+            getNextItem={getNextItem}
+            getPrevItem={getPrevItem}
+        />
+    );
 };
 
 export default SolarSystem;
