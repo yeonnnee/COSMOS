f8f680aad5ebe4a381cf00e2c381bdc4a407c0ab
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-11-28 01:13:58 +0900

    [CREATE] create DeatilContainer

---
+++ src/pages/Detail/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import DetailPresenter from "./DetailPresenter";
+import Data from "../../data/planets.json";
+
+const Detail: () => JSX.Element = () => {
+    const target = location.pathname.toString().split("/")[2];
+    const result = Data.filter((list) => list.name === target);
+
+    return <DetailPresenter planet={result} />;
+};
+export default Detail;
