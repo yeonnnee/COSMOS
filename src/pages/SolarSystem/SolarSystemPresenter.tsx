ca9c4e86e99811f1ded8489074ba9eda0b0b0b11
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-11-27 19:53:27 +0900

    [CREATE] create planet info data

--- src/pages/SolarSystem/SolarSystemPresenter.tsx
+++ src/pages/SolarSystem/SolarSystemPresenter.tsx
@@ -2,7 +2,26 @@ import React from "react";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { Carousel, Item, List, Main, Btn } from "./styles";
+import Mercury from "../../img/mercury.png";
+import Earth from "../../img/earth.jpg";
+import Mars from "../../img/mars.jpg";
+import Jupiter from "../../img/jupiter.jpg";
+import Saturn from "../../img/saturn.jpg";
+import Uranus from "../../img/uranus.jpg";
+import Neptune from "../../img/neptune.jpg";
+import Pluto from "../../img/pluto.jpg";
+import {
+    Carousel,
+    Item,
+    List,
+    Main,
+    Btn,
+    Img,
+    Info,
+    Name,
+    Desc,
+} from "./styles";
+
 interface Istate {
     currentGroup: number;
     left: number;
@@ -28,14 +47,62 @@ const SolarSystemPresneter: React.FunctionComponent<IProps> = (
             </Btn>
             <Carousel>
                 <List left={state.left}>
-                    <Item>MERCURY</Item>
-                    <Item>EARTH</Item>
-                    <Item>MARS</Item>
-                    <Item>JUPITER</Item>
-                    <Item>SATURN</Item>
-                    <Item>URANUS</Item>
-                    <Item>NEPTUNE</Item>
-                    <Item>PLUTO</Item>
+                    <Item>
+                        <Img imgUrl={Mercury}></Img>
+                        <Info>
+                            <Name>MERCURY</Name>
+                            <Desc>First planet from the Sun</Desc>
+                        </Info>
+                    </Item>
+                    <Item>
+                        <Img imgUrl={Earth}></Img>
+                        <Info>
+                            <Name>EARTH</Name>
+                            <Desc>Second planet from the Sun</Desc>
+                        </Info>
+                    </Item>
+                    <Item>
+                        <Img imgUrl={Mars}></Img>
+                        <Info>
+                            <Name>MARS</Name>
+                            <Desc>Third planet from the Sun</Desc>
+                        </Info>
+                    </Item>
+                    <Item>
+                        <Img imgUrl={Jupiter}></Img>
+                        <Info>
+                            <Name>JUPITER</Name>
+                            <Desc>Fourth planet from the Sun</Desc>
+                        </Info>
+                    </Item>
+                    <Item>
+                        <Img imgUrl={Saturn}></Img>
+                        <Info>
+                            <Name> SATURN</Name>
+                            <Desc>Fifth planet from the Sun</Desc>
+                        </Info>
+                    </Item>
+                    <Item>
+                        <Img imgUrl={Uranus}></Img>
+                        <Info>
+                            <Name>URANUS</Name>
+                            <Desc>Sixth planet from the Sun</Desc>
+                        </Info>
+                    </Item>
+                    <Item>
+                        <Img imgUrl={Neptune}></Img>
+                        <Info>
+                            <Name>NEPTUNE</Name>
+                            <Desc>Seventh planet from the Sun</Desc>
+                        </Info>
+                    </Item>
+                    <Item>
+                        <Img imgUrl={Pluto}></Img>
+                        <Info>
+                            <Name> PLUTO</Name>
+                            <Desc>Eighth planet from the Sun</Desc>
+                        </Info>
+                    </Item>
                 </List>
             </Carousel>
             <Btn onClick={getNextItem}>
--- src/pages/SolarSystem/styles.tsx
+++ src/pages/SolarSystem/styles.tsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
-
+import { Link } from "react-router-dom";
 interface IMove {
     left: number;
+}
+
+interface ImageProps {
+    imgUrl: string;
 }
 
 export const Main = styled.main`
@@ -24,12 +28,32 @@ export const List = styled.ul`
     grid-template-rows: 500px;
     gap: 20px;
     left: ${(props: IMove) => props.left + `px`};
-
     transition: left 0.5s ease-out;
 `;
 
 export const Item = styled.li`
     border: 1px solid white;
+    padding: 10px;
+    width: 100%;
+    height: 100%;
+    background-color: #d1ccc0;
+    /* display: flex;
+    align-items: center;
+    justify-content: center; */
+    cursor: pointer;
+    &:hover {
+        background-color: black;
+        /* display: block; */
+    }
+`;
+
+export const Img = styled.div`
+    height: 330px;
+    background-image: url(${(props: ImageProps) => props.imgUrl});
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 export const Btn = styled.span`
     padding: 5px 15px;
@@ -39,4 +63,24 @@ export const Btn = styled.span`
     &:hover {
         border: 2px solid #ffeaa7;
     }
+`;
+
+export const Info = styled.section`
+    text-align: center;
+    margin-top: 30px;
+`;
+
+export const Name = styled.div`
+    color: #5e5037;
+    font-size: 20px;
+    font-family: "Karla", sans-serif;
+    font-weight: bold;
+    letter-spacing: 2px;
+`;
+export const Desc = styled.div`
+    font-size: 18px;
+    line-height: 1.5;
+    color: #747069;
+    margin-top: 20px;
+    font-family: "Caveat", cursive;
 `;
