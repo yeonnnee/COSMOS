0916057d728ddcd7786aaf2f77296865bb662a21
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-11-28 22:37:13 +0900

    [REVISE] revise carousel slide style,when the button clicked show one different item before,but now show 4 different items instead

--- src/pages/Planets/index.tsx
+++ src/pages/Planets/index.tsx
@@ -12,10 +12,17 @@ const Planets: () => JSX.Element = () => {
     const [data, setData] = useState([{ name: "", imgUrl: "", desc: "" }]);
 
     function getNextItem() {
-        if (state.currentGroup < info.length - 3) {
+        if (state.currentGroup === 1) {
             setState({
                 currentGroup: state.currentGroup + 1,
-                left: -state.currentGroup * 350 + -20 * state.currentGroup,
+                left: -state.currentGroup * 350 * 4 + -60,
+                max: false,
+                min: false,
+            });
+        } else if (state.currentGroup === 2) {
+            setState({
+                currentGroup: state.currentGroup + 1,
+                left: state.left - 370,
                 max: false,
                 min: false,
             });
@@ -28,6 +35,7 @@ const Planets: () => JSX.Element = () => {
             });
         }
     }
+
     function getPrevItem() {
         if (state.currentGroup === 1) {
             setState({
@@ -35,6 +43,13 @@ const Planets: () => JSX.Element = () => {
                 left: state.left,
                 max: false,
                 min: true,
+            });
+        } else if (state.currentGroup === 2) {
+            setState({
+                currentGroup: state.currentGroup - 1,
+                left: state.left + 370 * 4,
+                max: true,
+                min: false,
             });
         } else {
             setState({
@@ -45,6 +60,7 @@ const Planets: () => JSX.Element = () => {
             });
         }
     }
+
     function fetchData() {
         const planetList = [];
         for (let i = 0; i < info.length; i++) {
