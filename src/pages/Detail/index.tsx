e2b0cc5d71b61d684108fd2635b6243a5f144ce6
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-12-03 23:53:14 +0900

    [REVISE] create getPlanet func and get data form firestore

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DetailPresenter from "./DetailPresenter";
-import Data from "../../data/planets.json";
+
+import { db } from "../../firebase";
+
+export interface IDiameter {
+    equatorialDiameter: string;
+    polarDiameter: string;
+}
+
+export interface IPlanet {
+    name?: string;
+    imgUrl?: string;
+    desc?: string;
+    detail?: string;
+    mass?: string;
+    diameter?: string | IDiameter;
+    orbitPeriod?: string;
+    lengthOfDay?: string;
+    rotationPeriod?: string;
+    gravity?: string;
+    surfaceTemperature?: string;
+}
 
 const Detail: () => JSX.Element = () => {
-    const target = location.pathname.toString().split("/")[3];
-    const result = Data.filter((list) => list.name === target);
+    const [planet, setPlanet] = useState<IPlanet>();
 
-    return <DetailPresenter planet={result} />;
+    const getPlanet = async () => {
+        try {
+            const target = location.pathname.toString().split("/")[3];
+
+            const palnetRef = db.collection("planets").doc(`${target}`);
+            const doc = await palnetRef.get();
+
+            if (doc.exists) {
+                const result = doc.data();
+                setPlanet({
+                    name: result?.name,
+                    detail: result?.detail,
+                    imgUrl: result?.imgUrl,
+                    orbitPeriod: result?.orbitPeriod,
+                    lengthOfDay: result?.lengthOfDay,
+                    rotationPeriod: result?.rotationPeriod,
+                    surfaceTemperature: result?.surfaceTemperature,
+                    gravity: result?.gravity,
+                });
+            } else {
+                console.log("No such document!");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getPlanet();
+    }, []);
+
+    return <DetailPresenter planet={planet} />;
 };
 export default Detail;
