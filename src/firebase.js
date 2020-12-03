f1a160b6029f49fbd48a16d022dbc6ea25627ebb
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-12-03 23:51:45 +0900

    [CREATE] create database

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import firebase from "firebase/app";
+import "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -11,3 +12,5 @@ const firebaseConfig = {
 };
 
 export default firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
