993487c810825bdf933a83ff206e7040a67e91ad
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-12-09 22:35:24 +0900

    [DELETE] delete files

--- src/pages/SearchTickets/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import SearchTicketsPresentation from "./SearchTicketsPresentation";
-
-const SearchTickets: () => JSX.Element = () => {
-    return <SearchTicketsPresentation />;
-};
-
-export default SearchTickets;
