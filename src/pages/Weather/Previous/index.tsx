398ddb8295ec76c0ac6a96e9b6dbe8de4267fabd
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-12-09 20:57:02 +0900

    [CREATE] create Previous Data Component seperately from weather Pesenter

--- src/pages/Weather/Previous/index.tsx
+++ src/pages/Weather/Previous/index.tsx
@@ -1,46 +1,8 @@
 import React from "react";
 import { ISolData } from "../index";
-import styled from "styled-components";
 
-import { SolNum, Temperature, Data } from "../WeatherPresenter";
-
-const Previous = styled.section`
-    padding: 20px;
-    margin-top: 20px;
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
-    gap: 20px;
-    place-items: center;
-    text-align: center;
-    color: black;
-`;
-const Item = styled.div`
-    width: 100%;
-    height: 100%;
-    padding: 20px;
-`;
-
-const PrevDate = styled.div`
-    font-family: "Texturina", serif;
-    margin-top: 5px;
-`;
-
-const More = styled.button`
-    outline: none;
-    border: none;
-    box-shadow: 1px 1px 3px black;
-    /* background-color: #503c42; */
-    color: black;
-    padding: 2px 10px;
-    transform: translateY(20px);
-    border-radius: 3px;
-    font-family: "Karla", sans-serif;
-    font-size: 15px;
-    cursor: pointer;
-    &:hover {
-        box-shadow: 2px 2px 3px black;
-    }
-`;
+import { Previous, Item, More, PrevDate } from "./styles";
+import { SolNum, Temperature, Data, TempText, DataText } from "../styles";
 
 interface PreviousDataProps {
     previous: Array<ISolData>;
@@ -61,14 +23,12 @@ const PreviousData: React.FunctionComponent<PreviousDataProps> = (
                             {sol.maxTemp ? (
                                 <>
                                     <Data>
-                                        High:
-                                        {sol.maxTemp}
-                                        °C
+                                        <TempText> High :</TempText>
+                                        <DataText>{sol.maxTemp} °C</DataText>
                                     </Data>
                                     <Data>
-                                        Low:
-                                        {sol.minTemp}
-                                        °C
+                                        <TempText> Low :</TempText>
+                                        <DataText> {sol.minTemp} °C</DataText>
                                     </Data>
                                 </>
                             ) : (
