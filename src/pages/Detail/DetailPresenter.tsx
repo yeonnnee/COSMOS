db1b12d9e1be31422280f7ef2db0f8438a7bfafb
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-12-11 16:06:23 +0900

    [DELETE] delete unused bar

--- src/pages/Detail/DetailPresenter.tsx
+++ src/pages/Detail/DetailPresenter.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { IPlanet, IPlanets } from "./index";
 import {
